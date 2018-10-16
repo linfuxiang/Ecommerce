f9c5b79b2381fe9a31d37da91866d1d7da7264e6
linfuxiang <linfuxiang@git.jinxidao.com>
2018-10-16 21:23:20 +0800

    add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
     <div class="app">
-        <router-view></router-view>
+        {{ name }}
+        <!-- {{ nameA }} -->
+        <!-- {{ nameB }} -->
+        <button @click="increment">increment</button>
+        <!-- <router-view></router-view> -->
         <!-- <img src="@/assets/logo@2x.png" alt="" v-for="i in 50"> -->
     </div>
 </template>
 <script>
     // import iconfont from '../public/iconfont/iconfont.css'
+    // import { mapState } from 'vuex'
+
+    import { createNamespacedHelpers } from 'vuex'
+    const { mapState } = createNamespacedHelpers('moduleA')
     export default {
         data() {
             return {}
         },
+        computed: {
+            // ...mapState('moduleA', {
+            //     nameA: 'name',
+            //     // b: state => state.b
+            // })
+            ...mapState(['name'])
+        },
+        watch: {
+            name: {
+                deep: true,
+                handler(newV, oldV) {
+                    console.log(oldV, '===>', newV)
+                }
+            }
+        },
         methods: {
             boardcast() {
                 this.bus.$emit('boardcast', 'hey my sons.')
+            },
+            increment() {
+                this.$store.dispatch('moduleA/increment')
             },
         },
         mounted() {
@@ -22,8 +48,7 @@
             //  }
             // })
         },
-    }
+    };
 </script>
 <style lang="scss">
-
 </style>
--- src/stores/store.js
+++ src/stores/store.js
@@ -3,14 +3,72 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const moduleA = {
+    namespaced: true,
     state: {
-
+        // name: {
+        	name: 'A',
+        // },
+        count: 0,
     },
     mutations: {
+        increment(state) {
+            state.name += 'A'
+        }
+    },
+    getters: {
 
+    },
+    actions: {
+        increment({ state, commit, rootState, rootGetters }) {
+            commit('increment')
+            setTimeout(() => { commit('increment') }, 2000)
+            // commit('moduleB/increment', null, { root: true })
+        }
+    }
+}
+
+const moduleB = {
+    namespaced: true,
+    state: {
+        name: 'B',
+        count: 0,
+    },
+    getters: {
+        nnn() {
+            return 'BBB'
+        },
+    },
+    mutations: {
+        increment(state) {
+            state.name += 'B'
+        }
     },
     actions: {
 
     }
+}
+
+const store = new Vuex.Store({
+    modules: {
+        moduleA,
+        moduleB
+    }
 })
+
+// store.subscribe((mutation) => {
+//     console.log(mutation)
+// })
+// store.subscribeAction((mutation) => {
+//     console.log(mutation)
+// })
+store.watch(
+    (state) => state.moduleA.name,
+    (val1, val2) => {console.log(val1, val2)}
+)
+// console.log(subs)
+// setTimeout(()=>{
+// 	subs()
+// }, 5000)
+
+export default store
