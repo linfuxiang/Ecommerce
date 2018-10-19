281fc0aa974b98c6ec3f7fcc763cbcbd1738042f
linfuxiang <linfuxiang@git.jinxidao.com>
2018-10-19 18:43:56 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
         <!-- {{ nameA }} -->
         <!-- {{ nameB }} -->
         <button @click="increment">increment</button>
+        <select name="" id="">
+            <option value="i" v-for="i in obj.arr">{{ i }}</option>
+        </select>
+        <Home ref="home" :name="mmm"></Home>
         <!-- <router-view></router-view> -->
         <!-- <img src="@/assets/logo@2x.png" alt="" v-for="i in 50"> -->
     </div>
@@ -11,12 +15,20 @@
 <script>
     // import iconfont from '../public/iconfont/iconfont.css'
     // import { mapState } from 'vuex'
-
+    import Home from './views/Home'
     import { createNamespacedHelpers } from 'vuex'
     const { mapState } = createNamespacedHelpers('moduleA')
     export default {
         data() {
-            return {}
+            return {
+                obj: {
+                    arr: [1, 2, 3, 4, 5, 6]
+                },
+                mmm: '123',
+            }
+        },
+        components: {
+            Home
         },
         computed: {
             // ...mapState('moduleA', {
@@ -38,7 +50,10 @@
                 this.bus.$emit('boardcast', 'hey my sons.')
             },
             increment() {
-                this.$store.dispatch('moduleA/increment')
+                // this.$store.dispatch('moduleA/increment')
+                // this.obj.arr.splice(0, 1);
+                this.mmm = '456';
+                this.$refs.home.$emit('ttt');
             },
         },
         mounted() {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+        {{ name }}
+        <transition name="slide">
+            <div v-if="show">123</div>
+        </transition>
+    </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    // import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        data() {
+            return {
+                show: false
+            }
+        },
+        components: {},
+        props: ['name'],
+        mounted() {
+            this.$on('ttt', res => {
+                this.show = !this.show;
+                console.log(this.name);
+            })
+        },
+    };
 </script>
+<style scoped="">
+    .slide-enter-active {
+        /*transition: all 2s;*/
+        animation: bounce 1s;
+    }
+    .slide-leave-active {
+    	animation: bounce 1s reverse;
+    }
+    .slide-enter-to,
+    .slide-leave {
+        opacity: 0.5;
+    }
+    @keyframes bounce {
+    	0% {
+    		opacity: 0;
+    	}
+    	50% {
+    		opacity: 1;
+    	}
+    	100% {
+    		opacity: 0.5;
+    	}
+    }
+</style>
